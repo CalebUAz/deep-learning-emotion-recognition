798c03965c3199185a38f803eab92547baac8509
CalebUAz <calebshibu@email.arizona.edu>
2022-10-17 17:05:57 -0700

    module loading error fix

--- for_gauss/utils/__init__.py
+++ for_gauss/utils/__init__.py
@@ -1,3 +1,3 @@
-from hyperparameters import Config
-from loading_cv_data import loading_cv_data
-from torch_nn import cca_metric_derivative, AttentionFusion, TransformLayers, DCCA_AM
+from .hyperparameters import Config
+from .loading_cv_data import loading_cv_data
+from .torch_nn import cca_metric_derivative, AttentionFusion, TransformLayers, DCCA_AM
